d4a0e717eb154cc7c196134b3ad4c74c84abfc7d
Tengda Han <htd95@126.com>
2017-01-13 10:03:39 +1100

    Add files via upload

--- utils/simple_utils.py
+++ utils/simple_utils.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_batch(model, batch_size, sketch, color, epoch, idx, tag, nb_img=5):
+def plot_batch(model, img_size, batch_size, sketch, color, epoch, idx, tag, nb_img=5):
     img_sketch = np.array(sketch[0:nb_img])
     img_color = np.array(color[0:nb_img])
     img_gen = model.predict(sketch, batch_size=batch_size)[0][0:nb_img]
     for i in range(nb_img):
         plt.subplot(nb_img, 3, i * 3 + 1)
-        plt.imshow(img_sketch[i].reshape((64,64)), cmap='Greys_r')
+        plt.imshow(img_sketch[i].reshape((img_size,img_size)), cmap='Greys_r')
         plt.axis('off')
         plt.subplot(nb_img, 3, i * 3 + 2)
         plt.imshow(img_color[i])
