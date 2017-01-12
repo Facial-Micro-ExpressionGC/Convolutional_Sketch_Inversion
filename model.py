b00229e4570e3ba3f65a01c3386d44162b505b7a
Tengda Han <htd95@126.com>
2017-01-13 10:03:00 +1100

    Add files via upload

--- model.py
+++ model.py
@@ -19,11 +19,22 @@ def pixel_loss(y_true, y_pred):
     return K.sqrt(K.mean(K.square(y_true - y_pred)))
 
 
-def train(batch_size, n_batch_per_epoch, nb_epoch, sketch, color, weights, tag, save_weight=1):
-    opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
-    model, model_name = edge2color([64,64,1], batch_size=batch_size)
+def variation_loss(y_true, y_pred):
+    # Assume img size is 64*64
+    if K.image_dim_ordering() == 'tf':
+        a = K.square(y_pred[:, :64-1, :64-1, :] - y_pred[:, 1:, :64-1, :])
+        b = K.square(y_pred[:, :64-1, :64-1, :] - y_pred[:, :64-1, 1:, :])
+    else:
+        a = K.square(y_pred[:, :, 64 - 1, :64 - 1] - y_pred[:, :, 1:, :64 - 1])
+        b = K.square(y_pred[:, :, 64 - 1, :64 - 1] - y_pred[:, :, :64 - 1, 1:])
+    return K.sum(K.sqrt(a+b))
 
-    model.compile(loss=[pixel_loss, feature_loss], loss_weights=[5, 1], optimizer=opt)
+
+def train(batch_size, n_batch_per_epoch, nb_epoch, sketch, color, weights, tag, save_weight=1, img_dim=[64,64,1]):
+    opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
+    model, model_name = edge2color(img_dim, batch_size=batch_size)
+
+    model.compile(loss=[pixel_loss, feature_loss], loss_weights=[1, 1], optimizer=opt)
     model.summary()
     from keras.utils.visualize_util import plot
     plot(model, to_file='figures/edge2color.png', show_shapes=True, show_layer_names=True)
@@ -45,7 +56,7 @@ def train(batch_size, n_batch_per_epoch, nb_epoch, sketch, color, weights, tag, 
             progbar.add(batch_size, values=[('pixel_loss', train_loss[0]), ('feature_loss', train_loss[1])])
             # if batch_counter >= n_batch_per_epoch:
             if global_counter % 50 == 1:
-                plot_batch(model, batch_size, batch_sk, batch_co, epoch, idx, tag)
+                plot_batch(model, img_dim[0], batch_size, batch_sk, batch_co, epoch, idx, tag)
             global_counter += 1
 
             if batch_counter >= n_batch_per_epoch:
@@ -62,6 +73,8 @@ def train(batch_size, n_batch_per_epoch, nb_epoch, sketch, color, weights, tag, 
 
 
 if __name__ == '__main__':
-    sketch, color, weights = load(os.path.expanduser('~/Desktop/hdf5/clear/database_train.h5'))
-    train(batch_size=64, n_batch_per_epoch=1, nb_epoch=1000, sketch=sketch, color=color,
-          weights=weights, tag='5-1-1batch', save_weight=0)
+    sketch, color, weights = load(os.path.expanduser
+                                 ('~/Desktop/hdf5/clear/database_train.h5'))
+                                 # ('~/Desktop/DeepLearningImplementations/DFI/data/processed/lfw_224_data.h5'))
+    train(batch_size=64, n_batch_per_epoch=1, nb_epoch=10000, sketch=sketch, color=color,
+          weights=weights, tag='1-1-64batch-64-22', save_weight=0, img_dim=[64,64,1])
