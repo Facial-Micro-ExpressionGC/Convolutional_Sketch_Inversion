3aa1c75a7992e95670df29a27c08ddb01e51a591
Tengda Han <htd95@126.com>
2017-01-20 00:54:34 +1100

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -65,7 +65,7 @@ def build_HDF5(size):
     hdf5_file = os.path.join(data_dir, "lfw_%s_data.h5" % size)
     with h5py.File(hdf5_file, "w") as hfw:
 
-        data_color = hfw.create_dataset("data",
+        data_color = hfw.create_dataset("lfw_%s_color" % size,
                                         (0, 3, size, size),
                                         maxshape=(None, 3, size, size),
                                         dtype=np.uint8)
@@ -91,6 +91,39 @@ def build_HDF5(size):
             data_color.resize(data_color.shape[0] + arr_img_color.shape[0], axis=0)
 
             data_color[-arr_img_color.shape[0]:] = arr_img_color.astype(np.uint8)
+
+
+def compute_vgg(size, batch_size=32):
+    """
+    get VGG feature
+    """
+    from keras.applications import vgg16
+    from keras.applications.imagenet_utils import preprocess_input
+    from keras.models import Model
+
+    # load data
+    hdf5_file = os.path.join(data_dir, "lfw_%s_data.h5" % size)
+    with h5py.File(hdf5_file, "a") as hf:
+        X = hf["lfw_%s_color" % size][:].astype(np.float32)
+        X = preprocess_input(X)
+        # compute features
+        base_model = vgg16.VGG16(weights='imagenet', include_top=False)
+        model = Model(input=base_model.input, output=base_model.get_layer('block2_conv2').output)
+        vgg16_feat = model.predict(X, batch_size=batch_size, verbose=1)
+        hf.create_dataset('lfw_%s_vgg' % size, data=vgg16_feat)
+
+
+def get_bw(size):
+    """
+    get black and white images
+    """
+    # load data
+    hdf5_file = os.path.join(data_dir, "lfw_%s_data.h5" % size)
+    with h5py.File(hdf5_file, "a") as hf:
+        img = hf["lfw_%s_color" % size][:].astype(np.float32).transpose((0,2,3,1))
+        bw = np.dot(img[...,:3], [0.299, 0.587, 0.114])
+        print(bw.shape, 'is the shape of B&W images')
+        hf.create_dataset('lfw_%s_bw' % size, data=bw)
 
 
 def check_HDF5(size):
@@ -122,7 +155,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build dataset')
     parser.add_argument("keras_model_path", type=str,
                         help="Path to keras deep-learning-models directory")
-    parser.add_argument('--img_size', default=200, type=int,
+    parser.add_argument('--img_size', default=64, type=int,
                         help='Desired Width == Height')
     parser.add_argument('--do_plot', default=False, type=bool,
                         help='Whether to visualize saved images')
@@ -138,6 +171,8 @@ if __name__ == '__main__':
             os.makedirs(d)
 
     build_HDF5(args.img_size)
+    get_bw(args.img_size)
+    compute_vgg(args.img_size)
 
     if args.do_plot:
         check_HDF5(args.img_size)
